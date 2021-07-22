58ce1d8851788785676328ab39343f1b67a32a47
naveenjangam2327 <87269772+naveenjangam2327@users.noreply.github.com>
2021-07-22 15:58:08 +0530

    Add files via upload

---
+++ rules_voilated.py
@@ -0,0 +1,17 @@
+f=open('C:\\Users\\J.NAVEEN KUMAR\\Desktop\\vspython\\sample.log')
+x=f.readlines()
+count=0
+l=[]
+res=[]
+s=""
+for i in x:
+    if 'rules violated' in i:
+        l.append(i)
+for i in l:
+    for j in i.split():
+        if j.isdigit():
+            res.append(j)
+for i in res:
+    count=count+int(i)
+print("rules violated:",count)
+
